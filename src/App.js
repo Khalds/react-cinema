b3446cceb588830baaa19a0d96f4739797d27b6c
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-07-31 14:39:04 +0300

    Внёс поправки в CSS-код

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import Personal from "./pages/Personal/Personal"
+import Personal from "./pages/Personal/Personal";
 
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Homepage/Home"
-import Cinema from "./Components/Cinema/Cinema"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Homepage/Home";
+import Cinema from "./Components/Cinema/Cinema";
 
-import "./App.css"
-import BookingPage from "./pages/Bookingpage/Bookingpage"
-import Sessionspage from "./pages/Sessionspage/Sessionspage"
-import Movie from "./pages/Movie/Movie"
+import "./App.css";
+import BookingPage from "./pages/Bookingpage/Bookingpage";
+import Sessionspage from "./pages/Sessionspage/Sessionspage";
+import Movie from "./pages/Movie/Movie";
 import Basket from "./Components/Basket/Basket";
 
-import AboutUsPages from "./pages/AboutUs/AboutUsPages"
-import Authorization from "./pages/Authorization/AuthorizationPages"
-import Registration from "./pages/Registration/RegistrationPages"
+import AboutUsPages from "./pages/AboutUs/AboutUsPages";
+import Authorization from "./pages/Authorization/AuthorizationPages";
+import Registration from "./pages/Registration/RegistrationPages";
 
-import Sessionspage from "./pages/Sessionspage/Sessionspage"
-import Movie from "./pages/Movie/Movie"
-import Bookingpage from "./pages/Bookingpage/Bookingpage"
+
 
 function App() {
   return (
@@ -30,7 +28,6 @@ function App() {
         <Route path="/contact" element={<Contacts />} />
         <Roite path="/aboutus" element={<Aboutus />} /> */}
 
-        <Route path="/booking" element={<Bookingpage />} />
         <Route path="/cinema" element={<Cinema />} />
         <Route path="/movie/:id" element={<Movie />} />
         <Route path="/personal" element={<Personal />} />
@@ -39,7 +36,7 @@ function App() {
         <Route path="/aboutUs" element={<AboutUsPages />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/features/Sessions/sessionSlice.jsx
+++ src/features/Sessions/sessionSlice.jsx
@@ -1,42 +1,37 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    sessions: [],
-    session: null
-};
-
-export const getSessions = createAsyncThunk("get/sessions", async (id, thunkAPI) => {
-  try {
-    const res = await fetch("http://localhost:4000/session");
-    const data = await res.json();
   sessions: [],
-}
+  session: null,
+};
 
 export const getSessions = createAsyncThunk(
   "get/sessions",
   async (thunkAPI) => {
     try {
-      const res = await fetch("http://localhost:4000/session")
-      const data = await res.json()
+      const res = await fetch("http://localhost:4000/session");
+      const data = await res.json();
 
-      return data
+      return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message)
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
-)
+);
 
-export const getSessionById = createAsyncThunk("get/sessionById", async (id, thunkAPI) => {
-  try {
-    const res = await fetch(`http://localhost:4000/session/${id}`);
-    const data = await res.json();
+export const getSessionById = createAsyncThunk(
+  "get/sessionById",
+  async (id, thunkAPI) => {
+    try {
+      const res = await fetch(`http://localhost:4000/session/${id}`);
+      const data = await res.json();
 
-    return data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-});
-
+);
 
 export const sessionSlice = createSlice({
   name: "sessions",
@@ -44,15 +39,13 @@ export const sessionSlice = createSlice({
   reducer: {},
   extraReducers: (builder) => {
     builder
-    .addCase(getSessions.fulfilled, (state, action) => {
-      state.sessions = action.payload;
-    })
-    .addCase(getSessionById.fulfilled, (state, action) => {
-      state.session = action.payload})
-    builder.addCase(getSessions.fulfilled, (state, action) => {
-      state.sessions = action.payload
-    })
+      .addCase(getSessions.fulfilled, (state, action) => {
+        state.sessions = action.payload;
+      })
+      .addCase(getSessionById.fulfilled, (state, action) => {
+        state.session = action.payload;
+      });
   },
-})
+});
 
-export default sessionSlice.reducer
+export default sessionSlice.reducer;
